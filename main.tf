08c6eb0f96972fb5af6f22fbf0a6c61953ff9e9f
thomas punnala <thomas.punnala@hotmail.fi>
2026-03-28 17:54:27 +0200

    add force destroy for buckets

--- main.tf
+++ main.tf
@@ -1,9 +1,11 @@
 resource "aws_s3_bucket" "input" {
   bucket = "file-pipeline-input-${data.aws_caller_identity.current.account_id}"
+  force_destroy = True
 }
 
 resource "aws_s3_bucket" "output" {
   bucket = "file-pipeline-output-${data.aws_caller_identity.current.account_id}"
+  force_destroy = True
 }
 
 data "aws_iam_role" "labrole" {
